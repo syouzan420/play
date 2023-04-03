6cb65d1d213229e43da6d4d4172f26f88adc7eda
syouzan420 <syouzanponkikki@yahoo.co.jp>
2023-04-03 23:46:43 +0900

    update

--- eigotex/eigo.sh
+++ eigotex/eigo.sh
@@ -1,8 +1,13 @@
+ghc eigo.hs
 ./eigo $1 $2
 uplatex -src -interaction=nonstopmode "eQuestion".tex
 uplatex -src -interaction=nonstopmode "eAnswer".tex
 dvipdfmx "eQuestion".dvi
 dvipdfmx "eAnswer".dvi
+rm eigo.o
+rm eigo.hi
+rm Myfile.o
+rm Myfile.hi
 rm eQuestion.log
 rm eQuestion.aux
 rm eQuestion.dvi

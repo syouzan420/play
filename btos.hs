756442a9d1b00ea3e5e734247b67d852ef1aaeec
syouzan420 <syouzanponkikki@yahoo.co.jp>
2023-08-11 12:36:16 +0900

    update

--- btos.hs
+++ btos.hs
@@ -3,6 +3,10 @@
 import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Diagrams(toFile)
 
+type Phase = Integer
+type Cycle = Integer
+type Interval = (Phase,Cycle)
+
 main = toFile def "btos.svg" $ do
   layout_title .= "binary data to cos function "++show test++" "++show (intervals testb)
   setColors [opaque blue, opaque red]
@@ -28,21 +32,21 @@ isPeriodic x bls = let xi = fromIntegral x
 nextPTrue :: Integer -> [Bool] -> Integer
 nextPTrue x bls = if isPeriodic x bls then x else nextPTrue (x+1) bls
 
-interval :: Integer -> [Bool] -> (Integer,Integer)
+interval :: Integer -> [Bool] -> Interval 
 interval i bls
   | length bls - 1< fromIntegral i = (-1,0)
   | bls!!fromIntegral i = (i,nextPTrue 1 (drop (fromIntegral i) bls))
   | otherwise = interval (i+1) bls
 
-intervals :: [Bool] -> [(Integer,Integer)]
+intervals :: [Bool] -> [Interval]
 intervals bls = let intv = interval 0 bls
                     nbl = nextBoolList intv bls
                  in if intv==(-1,0) then [] else intv:intervals nbl
 
-nextBoolList :: (Integer,Integer) -> [Bool] -> [Bool]
+nextBoolList :: Interval -> [Bool] -> [Bool]
 nextBoolList = changeTF 0
 
-changeTF :: Integer -> (Integer,Integer) -> [Bool] -> [Bool] 
+changeTF :: Integer -> Interval -> [Bool] -> [Bool] 
 changeTF _ _ [] = []
 changeTF x (fsi,inv) bls
   | fromIntegral x > length bls - 1 = []
@@ -50,10 +54,14 @@ changeTF x (fsi,inv) bls
                                   = False:changeTF (x+1) (fsi,inv) bls
   | otherwise = (bls!!fromIntegral x):changeTF (x+1) (fsi,inv) bls
 
-intvToBool :: Integer -> [(Integer,Integer)] -> [Bool]
+binToBfunc :: [Bool] -> (Integer -> Bool)
+binToBfunc b i = let intvs = intervals b
+                  in intvToBool' intvs!!fromIntegral i
+
+intvToBool :: Integer -> [Interval] -> [Bool]
 intvToBool i intvs = take (fromIntegral i) (intvToBool' intvs)
 
-intvToBool' :: [(Integer,Integer)] -> [Bool]
+intvToBool' :: [Interval] -> [Bool]
 intvToBool' = foldr ((<+>).makeBool) []  
 
 (<+>) :: [Bool] -> [Bool] -> [Bool]
@@ -61,7 +69,7 @@ intvToBool' = foldr ((<+>).makeBool) []
 (<+>) bls [] = bls 
 (<+>) (b0:b0s) (b1:b1s) = (b0||b1):(b0s <+> b1s)
 
-makeBool :: (Integer,Integer) -> [Bool]
+makeBool :: Interval -> [Bool]
 makeBool (fsi,inv) = 
   replicate (fromIntegral fsi) False ++ cycle (True:replicate (fromIntegral inv-1) False)
 
@@ -69,7 +77,7 @@ binToCos :: [Bool] -> (Double -> Double)
 binToCos b = let intvs = intervals b
               in toCos intvs
 
-toCos :: [(Integer,Integer)] -> (Double -> Double)
+toCos :: [Interval] -> (Double -> Double)
 toCos invs x = foldl (\acc (fsi,inv) -> let fi=fromIntegral fsi; iv=fromIntegral inv in acc+(1/iv)^2*cos (((2*pi)/iv)*(x-fi))^2) 0 invs
 
 

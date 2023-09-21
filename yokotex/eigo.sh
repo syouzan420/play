5526b3b68c2f1210745a118895e49219f7f3b250
syouzan420 <syouzanponkikki@yahoo.co.jp>
2023-09-21 11:48:27 +0900

    update

--- yokotex/eigo.sh
+++ yokotex/eigo.sh
@@ -1,16 +1,18 @@
-ghc eigo.hs
-./eigo $@
-uplatex -src -interaction=nonstopmode "eQuestion".tex
-uplatex -src -interaction=nonstopmode "eAnswer".tex
-dvipdfmx "eQuestion".dvi
-dvipdfmx "eAnswer".dvi
-rm eigo.o
-rm eigo.hi
+ghc eigoMain.hs
+./eigoMain $@
+uplatex -src -interaction=nonstopmode "eigoQ".tex
+uplatex -src -interaction=nonstopmode "eigoA".tex
+dvipdfmx "eigoQ".dvi
+dvipdfmx "eigoA".dvi
+rm eigoMain.o
+rm eigoMain.hi
 rm Myfile.o
 rm Myfile.hi
-rm eQuestion.log
-rm eQuestion.aux
-rm eQuestion.dvi
-rm eAnswer.log
-rm eAnswer.aux
-rm eAnswer.dvi
+rm eigoQ.log
+rm eigoQ.aux
+rm eigoQ.dvi
+rm eigoQ.tex
+rm eigoA.log
+rm eigoA.aux
+rm eigoA.dvi
+rm eigoA.tex
